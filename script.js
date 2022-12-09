27b6e0d1fa273f252ef15ef745108a8aa85234df
Ahmed-Techie <sudheer.ai@yahoo.com>
2022-12-09 12:11:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,105 @@
+//1.     Solving problems using array functions on the rest countries' data.
+//          a. Get all the countries from the Asia continent /region using the Filter function
+//          b. Get all the countries with a population of less than 2 lakhs using Filter function
+//          c. Print the following details name, capital, flag using forEach function
+//          d. Print the total population of countries using reduce function
+//          e. Print the country which uses US Dollars as currency.
+/**********************************************************************************************/
+
+/***********a. Get all the countries from the Asia continent /region using the Filter function******/
+
+        var request = new XMLHttpRequest();
+        request.open("GET","https://restcountries.com/v3.1/all",true)
+        request.send()
+        request.onload = function() {
+            var data = request.response
+            var result = JSON.parse(data)
+            var res = result.filter((ele)=>ele.region==="Asia")
+            var res1 = res.map((ele)=>ele.name.common)
+            console.log(res1);
+
+        //Method1-display
+
+        for(var i of res1) {
+            console.log(res1[i]);
+        }
+        console.log("The countries from the Asia continent");
+        for(var j in res1) {
+            console.log(res1[j]);
+        }
+
+        }
+/**********b. Get all the countries with a population of less than 2 lakhs using Filter function*****/
+
+var request1 = new XMLHttpRequest();
+request1.open("GET","https://restcountries.com/v3.1/all",true)
+request1.send()
+request1.onload = function() {
+    var data = request1.response
+    var result = JSON.parse(data)
+    var res = result.filter((ele)=>ele.population>200000)
+    var res1 = res.map((ele)=>ele.name.common)
+   console.log(res1);
+
+    //Method2-display
+
+for(var k of res1) {
+    console.log(res1[k])
+}
+console.log("Countries with a population of less than 2 lakhs");
+for(var l in res1) {
+    console.log(res1[l]);
+}
+
+}
+                                                                            
+/***********c. Print the following details name, capital, flag using forEach function**************************************************************************/
+
+
+var request2 = new XMLHttpRequest();
+request2.open("GET","https://restcountries.com/v3.1/all",true)
+request2.send()
+ request2.onload=function(){
+    console.log("Print the following details name, capital, flag using forEach function");
+    var data = request2.response
+    var result = JSON.parse(data)
+    var res = result.forEach((ele)=>{console.log(`${ele.name.common} ${ele.capital} ${ele.region}`)})
+    
+}
+
+
+
+
+/************d. Print the total population of countries using reduce function*********************************************************************************/
+
+var request3 = new XMLHttpRequest();
+request3.open("GET","https://restcountries.com/v3.1/all",true)
+request3.send()
+ request3.onload=function(){
+    var data = request3.response
+    var result = JSON.parse(data)
+    var res = result.reduce((acc,cv)=>acc+cv.population,0)
+    console.log(`Total population of countries using reduce function ${res}`);
+    }
+
+
+/************e. Print the country which uses US Dollars as currency.*********************************************************************************/
+        
+var request4 = new XMLHttpRequest();
+request4.open("GET","https://restcountries.com/v2/all",true)
+request4.send()
+request4.onload = function() {
+    var data = request4.response
+    var result = JSON.parse(data)
+
+    var res = result.filter((ele)=>{
+        for(let key in ele.currencies) {
+            if(ele.currencies[key].code==="USD") {
+                console.log(ele.name)
+            }
+        }
+    })
+
+}
+
+/***************************Task - End********************************************************/
